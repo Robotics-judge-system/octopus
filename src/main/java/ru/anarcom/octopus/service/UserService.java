e9b4e05c5fc228097f5e383bd0c7f7ffdd24ccc2
Alexander Dolgiy <alex20021505@gmail.com>
2021-09-08 11:56:35 +0700

    deleted unimportant javadoc

--- src/main/java/ru/anarcom/octopus/service/UserService.java
+++ src/main/java/ru/anarcom/octopus/service/UserService.java
@@ -4,13 +4,6 @@ package ru.anarcom.octopus.service;
 import java.util.List;
 import ru.anarcom.octopus.model.User;
 
-/**
- * Service interface for class {@link User}.
- *
- * @author Eugene Suleimanov
- * @version 1.0
- */
-
 public interface UserService {
 
     List<User> getAll();
