147d4a991eec4d4d77183ec3423fbe40ecbac221
Alexander Dolgiy <alex20021505@gmail.com>
2021-10-20 23:34:52 +0700

    Added first step of CategoryController (with tests)

---
+++ src/main/kotlin/ru/anarcom/octopus/controller/competition/CategoryController.kt
@@ -0,0 +1,38 @@
+package ru.anarcom.octopus.controller.competition
+
+import org.springframework.web.bind.annotation.*
+import ru.anarcom.octopus.dto.competition.CategoryDto
+import ru.anarcom.octopus.facade.CategoryFacade
+
+@RestController
+@RequestMapping("api/v1/competition/{competition_id}/category")
+class CategoryController(
+    private val categoryFacade: CategoryFacade
+) {
+    @PostMapping("create")
+    fun createCategory(
+        @PathVariable("competition_id") competitionId: Long,
+        @RequestBody categoryDto: CategoryDto
+    ): CategoryDto = CategoryDto.fromCategory(
+        categoryFacade.addCategory(
+            categoryDto,
+            competitionId
+        )
+    )
+
+    @GetMapping("/all")
+    fun getCategoryForCompetition(
+        @PathVariable("competition_id") competitionId: Long,
+    ): List<CategoryDto> = CategoryDto.fromCategory(
+        categoryFacade.getByCompetition(competitionId)
+    )
+
+    @PostMapping("{category_id}/update")
+    fun updateCategoryForCompetition(
+        @PathVariable("competition_id") competitionId: Long,
+        @PathVariable("category_id") categoryId: Long,
+        @RequestBody categoryDto: CategoryDto
+    ): CategoryDto = CategoryDto.fromCategory(
+        categoryFacade.updateCategory(categoryId, competitionId, categoryDto)
+    )
+}
