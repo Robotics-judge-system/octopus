c7a3b972dca66ee1eef1778b3bc6608438b8635a
Миша Дубинин <m.dubinin1@g.nsu.ru>
2022-02-16 00:04:50 +0700

    added check for '/' in ResourceReader

--- src/test/java/ru/anarcom/octopus/utilus/ResourceReader.java
+++ src/test/java/ru/anarcom/octopus/utilus/ResourceReader.java
@@ -6,7 +6,10 @@ import java.nio.charset.StandardCharsets;
 
 public class ResourceReader {
     public static String getResource(String path) throws IOException {
-        InputStream inputstream = ClassLoader.getSystemResourceAsStream(path);
-        return new String(inputstream.readAllBytes(), StandardCharsets.UTF_8);
+        if (path.indexOf('/') == 0) {
+            path = path.substring(1);
+        }
+        InputStream is = ClassLoader.getSystemResourceAsStream(path);
+        return new String(is.readAllBytes(), StandardCharsets.UTF_8);
     }
 }
