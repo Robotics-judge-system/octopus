3a0a0460d0866f20290084e0124b8d7c6fba8325
Миша Дубинин <m.dubinin1@g.nsu.ru>
2022-02-21 20:25:35 +0700

    resourceReader from java to kotlin

---
+++ src/test/kotlin/ru/anarcom/octopus/utilus/ResourceReader.kt
@@ -0,0 +1,17 @@
+package ru.anarcom.octopus.utilus
+
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
+
+class ResourceReader {
+    @kotlin.jvm.Throws(IOException::class)
+    fun getResource(path : String): String{
+        var path = path
+        if(path.indexOf('/') == 0){
+            path = path.substring(1)
+        }
+        val tmp = ClassLoader.getSystemResourceAsStream(path)
+        return String(tmp.readAllBytes(), StandardCharsets.UTF_8)
+    }
+}
