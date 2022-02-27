d2319bff52ab2e0a7420203fea5daa775847215e
Alexander Dolgiy <alex20021505@gmail.com>
2022-02-27 12:34:16 +0700

    method changed to static

--- src/test/kotlin/ru/anarcom/octopus/utilus/ResourceReader.kt
+++ src/test/kotlin/ru/anarcom/octopus/utilus/ResourceReader.kt
@@ -5,13 +5,15 @@ import java.nio.charset.StandardCharsets
 
 
 class ResourceReader {
+companion object {
     @kotlin.jvm.Throws(IOException::class)
-    fun getResource(path : String): String{
+    fun getResource(path: String): String {
         var path = path
-        if(path.indexOf('/') == 0){
+        if (path.indexOf('/') == 0) {
             path = path.substring(1)
         }
         val tmp = ClassLoader.getSystemResourceAsStream(path)
         return String(tmp.readAllBytes(), StandardCharsets.UTF_8)
     }
+}
 }
