c494cd32aef9731a4a7162e4496e74de80d7548d
Alexander Dolgiy <alex20021505@gmail.com>
2022-03-13 14:38:54 +0700

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
 	testImplementation("org.mockito:mockito-all:1.10.19")
 	testImplementation("com.github.springtestdbunit:spring-test-dbunit:1.3.0")
-	testImplementation("org.dbunit:dbunit:2.7.2")
+	testImplementation("org.dbunit:dbunit:2.7.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-web:2.6.4")
 	implementation("com.google.guava:guava:31.0.1-jre")
 
