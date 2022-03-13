5599390c891ba6a1b9208693ad7532c1d301e0fb
Alexander Dolgiy <alex20021505@gmail.com>
2022-03-13 15:17:59 +0700

    Update version of junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,9 +57,10 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-web:2.6.4")
 	implementation("com.google.guava:guava:31.0.1-jre")
 
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-	testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+	testImplementation("org.junit.platform:junit-platform-commons:1.8.2")
 }
 
 tasks.withType<KotlinCompile> {
